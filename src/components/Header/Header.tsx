8a258c973bd3ef926f331b17f23f6f13d2317ef2
Simona Ragauskaitė <simonaaragauskaite@gmail.com>
2024-07-13 18:50:14 +0300

    added text style from google fonts

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -16,13 +16,13 @@ const Header: React.FC = () => {
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography variant="h6" sx={{ flexGrow: 1 }}>
-          My App
+        <Typography variant="h6" sx={{ flexGrow: 1, fontFamily: 'Jura', textTransform: 'none' }}>
+          SimonaRa20
         </Typography>
         <Box>
-          <Button color="inherit" onClick={() => handleNavigation('/projects')}>Projects</Button>
-          <Button color="inherit" onClick={() => handleNavigation('/art')}>Art</Button>
-          <Button color="inherit" onClick={() => handleNavigation('/')}>About Me</Button>
+          <Button color="inherit" sx={{fontFamily: 'Jura', textTransform: 'none'}} onClick={() => handleNavigation('/projects')}>Projects</Button>
+          <Button color="inherit" sx={{fontFamily: 'Jura', textTransform: 'none'}} onClick={() => handleNavigation('/art')}>Art</Button>
+          <Button color="inherit" sx={{fontFamily: 'Jura', textTransform: 'none'}} onClick={() => handleNavigation('/')}>About Me</Button>
         </Box>
       </Toolbar>
     </AppBar>
